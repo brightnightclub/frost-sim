d2b59b3b8ca28db3209d16459c3f0212137a5157
Ariel Spear, Sam Julien, Jason Dinsmore, Spydermonkee <simpleredfox@gmail.com>
2015-04-20 21:28:16 -0700

    Glass puts random vapor state.

--- gerda/windshield.rb
+++ gerda/windshield.rb
@@ -1,20 +1,71 @@
+require 'pry'
 class Windshield
-
   def initialize(width, height)
     @width = width
     @height = height
+    @glass = Array.new(@width){ Array.new(@height){ make_cell }}
+    # @cells = @width*@height
+    # @ghosts_wide = @width/2
+    # @ghosts_tall = @height/2
+    #inject_vapor
+    @glass[@width/2][@height/2] = Cell.new("*")
   end
 
-def print
-end
+  def make_cell
+    rand_state = [" ", "."].sample
+    Cell.new(rand_state)
+  end
 
+  # def inject_vapor
+  #   vapors = (@width * @height) / 2
+  #   vapors.times do
+  #     w = (0..@width-1).to_a.sample
+  #     h = (0..@height-1).to_a.sample
+  #     puts "w: #{w}. h: #{h}"
+  #     cell = @glass[w][h]
+  #     puts cell.state
+  #     cell.state = ['.', ' '].sample
+  #   end
+  # end
+
+  def print
+    @glass.each do |arr|
+      arr.each do |c|
+        putc c.state
+      end
+      puts
+    end
+  end
 end
 
 class Tick
+  def freezeSwirl
 
-def freezeSwirl
+  end
+
+  def shiftPane
+
+  end
 end
 
-def shiftPane
-
+class Cell
+  attr_accessor :state
+  def initialize(state)
+    @state = state
+  end
 end
+
+puts "Give us a width that is an even number, hahahahahahaha:"
+widput = gets.chomp.to_i
+while widput.zero? || widput.odd?
+  puts "Seriously. You don't listen well... Please, and EVEN number."
+  widput = gets.chomp.to_i
+end
+puts "Alright, now give us an even height for your windshield:"
+heightput = gets.chomp.to_i
+while heightput.zero? || heightput.odd?
+  puts "Smart ass.  I said an even number."
+  heightput = gets.chomp.to_i
+end
+our_windshield = Windshield.new(widput, heightput)
+our_windshield.print
