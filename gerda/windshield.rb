7b2a6c090996a637361a6ff06e2dc4fc8f6d16d3
Ariel Spear, Sam Julien, Jason Dinsmore, Spydermonkee <simpleredfox@gmail.com>
2015-04-20 20:03:34 -0700

    Initial Skeleton

---
+++ gerda/windshield.rb
@@ -0,0 +1,20 @@
+class Windshield
+
+  def initialize(width, height)
+    @width = width
+    @height = height
+  end
+
+def print
+end
+
+end
+
+class Tick
+
+def freezeSwirl
+end
+
+def shiftPane
+
+end
